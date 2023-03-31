a9577b197761f967e874307f9b1f22b8f62c1660
BK <forbodya@gmail.com>
2023-03-31 14:48:21 +0300

    Refactored code using ES modules with import

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,7 @@
-/// <reference path="components/project-item.ts"/>'
-/// <reference path="components/project-list.ts"/>'
-/// <reference path="components/project-input.ts"/>'
+import {ProjectInput} from "./components/project-input.js";
+import {ProjectList} from "./components/project-list.js";
 
+new ProjectInput();
+new ProjectList('active');
+new ProjectList('finished');
 
-namespace App {
-
-    new ProjectInput();
-    new ProjectList('active');
-    new ProjectList('finished');
-
-}
